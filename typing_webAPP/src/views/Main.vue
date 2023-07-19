4090d726e3f43576c33dee5c1b569e465d5e0230
Ayaka Kawabe <kawabe.ayaka@icloud.com>
2023-07-19 10:20:26 +0900

    [add]一つのタイピング問題

--- typing_webAPP/src/views/Main.vue
+++ typing_webAPP/src/views/Main.vue
@@ -1,49 +1,40 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 const typingStrList1:string[]=["bunnka","zyouhou","gakubu"];
 const typingStrList2:string[]=["doshisha","daigaku"];
 const userTyping=ref("");
 let typingStr_split:string[];
+let isUserTypingCorrect:boolean;
+
 typingStr_split=typingStrList1[0].split("");
-
-const typing_oneWord=(nowTypingIndex:number):boolean=>{
-    let isSame=false;
-    const isTypingKeyCorrect=(keyObj:KeyboardEvent):void=>{
-    const nowTypingStr:string=typingStr_split[nowTypingIndex];
-    const nowTyping_li=document.getElementById(nowTypingStr) as HTMLElement;
-    if(keyObj.key==nowTypingStr){
-        console.log(nowTypingStr);
-        nowTyping_li.style.color="blue";
-        isSame=true;
-    }
-    };
-    return isSame;
-};
-
-const typing_oneSentenece=():void=>{
-    let i=-1;
-    let isCorrect=true;
-    while(isCorrect==true){
-        i+=1;
-        isCorrect=typing_oneWord(i);
-    }
-};
-
-//typing_oneWord(1);
-
 const inputKeyboard=(keyObj:KeyboardEvent):void=>{
     userTyping.value+=keyObj.key;
+
 }
 document.addEventListener("keypress",inputKeyboard);
+
+watch(userTyping,()=>{
+    if(userTyping.value.length==typingStr_split.length){
+        if(userTyping.value==typingStrList1[0]){
+            isUserTypingCorrect=true;
+        }else{
+            isUserTypingCorrect=false;
+        }
+        console.log(isUserTypingCorrect);
+        document.removeEventListener("keypress",inputKeyboard);
+    }
+})
 
 
 </script>
 <template>
     <section>
-        <ul>
+        <ul class="question">
             <li v-for="str in typingStr_split" v-bind:id="str">{{ str }}</li>
         </ul>
-        <p>{{ userTyping[0] }}</p>
+        <ul class="answer">
+            <li v-for="str in userTyping" v-bind:id="'user'+str">{{ str }}</li>
+        </ul>
     </section>
 </template>
 
@@ -60,6 +51,12 @@ ul{
     padding: 50px;
     width: 100%;
     position: absolute;
-    top: 35vh;
+}
+.question{
+    top: 20vh;
+}
+.answer{
+    top:40vh;
+    color: blue;
 }
 </style>
